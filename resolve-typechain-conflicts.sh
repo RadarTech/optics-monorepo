3637ac499951011d43aa43d3ab3dcb4d4567d716
Erin Hales <mcbridee093@gmail.com>
2021-08-17 12:53:12 -0600

    refactor: all the test scripts

--- resolve-typechain-conflicts.sh
+++
@@ -1,8 +0,0 @@
-git rm -rf typescript/typechain/optics-xapps
-git rm -rf typescript/typechain/optics-core
-cd solidity/optics-core
-npm run compile
-cd ../optics-xapps
-npm run compile
-cd ../..
-git add typescript/typechain
---
+++ scripts/resolve-typechain-conflicts.sh
@@ -0,0 +1,6 @@
+cd ../solidity/optics-core
+npm run compile
+cd ../optics-xapps
+npm run compile
+cd ../..
+git add typescript/typechain
